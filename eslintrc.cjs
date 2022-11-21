4a9c55b2f851a27224cf7fd715e32fdedfb8a7e0
WARDI-Kamal <kha0s@free.fr>
2022-11-21 15:19:01 +0400

    build: Project settings
    Added and setup ESLint lib

---
+++ eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root:true,
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    'no-empty': 'warn'
+  },
+}
