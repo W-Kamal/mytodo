9aaa79d0270059d7eec83fbd9d704cf46949089d
WARDI-Kamal <kha0s@free.fr>
2022-11-30 13:18:26 +0400

    style: Files
    Changed data file extension, modified html page title

---
+++ src/assets/data.ts
@@ -0,0 +1,36 @@
+const todos = [
+  {
+    "id": 1,
+    "label": "Faire la vaisselle",
+    "created_at": "18/11/2022",
+    "edited_at":"",
+    "status": "pending",
+    "validated": false
+  },
+  {
+    "id": 2,
+    "label": "Passer l'aspi",
+    "created_at": "18/11/2022",
+    "edited_at":"25/11/2022",
+    "status": "validate",
+    "validated": true
+  },
+  {
+    "id": 3,
+    "label": "Jouer",
+    "created_at": "18/11/2022",
+    "edited_at":"",
+    "status": "pending",
+    "validated": false
+  },
+  {
+    "id": 4,
+    "label": "Régler le micro",
+    "created_at": "18/11/2022",
+    "edited_at":"",
+    "status": "cancelled",
+    "validated": false
+  }
+]
+
+export default todos;
